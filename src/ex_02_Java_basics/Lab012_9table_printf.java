7d68d4815723bdad003c3d57f3ca685c835f7af4
Lasya7893 <srilasyachangati@gmail.com>
2025-06-10 19:02:58 +0530

    10th june task

---
+++ src/ex_02_Java_basics/Lab012_9table_printf.java
@@ -0,0 +1,20 @@
+package ex_02_Java_basics;
+
+public class Lab012_9table_printf {
+    public static void main(String[] args) {
+        int a = 9;
+        System.out.println("The 9 table is as follows:");
+        System.out.printf("%d x 1 = %d.%n",a,a*1);
+        System.out.printf("%d x 2 = %d.%n",a,a*2);
+        System.out.printf("%d x 3 = %d.%n",a,a*3);
+        System.out.printf("%d x 4 = %d.%n",a,a*4);
+        System.out.printf("%d x 5 = %d.%n",a,a*5);
+        System.out.printf("%d x 6 = %d.%n",a,a*6);
+        System.out.printf("%d x 7 = %d.%n",a,a*7);
+        System.out.printf("%d x 8 = %d.%n",a,a*8);
+        System.out.printf("%d x 9 = %d.%n",a,a*9);
+        System.out.printf("%d x 10= %d.%n",a,a*10);
+        System.out.printf("%d x 11= %d.%n",a,a*11);
+        System.out.printf("%d x 12= %d.%n",a,a*12);
+    }
+}
