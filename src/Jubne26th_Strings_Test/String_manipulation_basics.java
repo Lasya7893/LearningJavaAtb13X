381db4f54a758bcbdf7bc7be0518028a72cc87d8
Lasya7893 <srilasyachangati@gmail.com>
2025-06-26 11:46:39 +0530

    26thJune test Challenge5

---
+++ src/Jubne26th_Strings_Test/String_manipulation_basics.java
@@ -0,0 +1,13 @@
+package Jubne26th_Strings_Test;
+
+public class String_manipulation_basics {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        System.out.println("length is " + str.length());
+        System.out.println(str + " Greetings");
+        System.out.println(str.substring(1,6));
+        System.out.println(str.toLowerCase());
+        System.out.println(str.charAt(0));
+        System.out.println(str.charAt(str.length()-1));
+    }
+}
